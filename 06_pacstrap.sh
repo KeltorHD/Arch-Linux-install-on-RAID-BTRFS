9f9fe7f188ac3ec45142221e53b9357021ffa7fc
Sebastian Majewski <49081304+ccie18643@users.noreply.github.com>
2022-03-01 10:51:48 -0600

    Update 06_pacstrap.sh

--- 06_pacstrap.sh
+++ 06_pacstrap.sh
@@ -4,5 +4,5 @@ set -Euo pipefail
 
 pacstrap /mnt base linux linux-firmware
 pacstrap /mnt vim grub grub-btrfs efibootmgr mdadm amd-ucode btrfs-progs sudo os-prober snapper snap-pac reflector openssh nftables
-pacstrap /mnt git man-db man-pages texinfo exfat-utils base-devel linux-headers dialog os-prober mtools dosfstools bridge-utils
+pacstrap /mnt git man-db man-pages texinfo exfat-utils base-devel linux-headers dialog os-prober mtools dosfstools bridge-utils archlinux-keyring
 
