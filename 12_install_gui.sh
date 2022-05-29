30f6f589d97de0a52223b087f8090aa00a4aa191
Sebastian Majewski <49081304+ccie18643@users.noreply.github.com>
2022-05-29 09:14:19 -0500

    Update 12_install_gui.sh

--- 12_install_gui.sh
+++ 12_install_gui.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+set -Euo pipefail
+
+pacman -S xorg xorg-server
+pacman -S plasma kde-applications
+pacman -S ttf-liberation ttf-fira-code
+pacman -S firefox
+pacman -S flatpak
+
 systemctl enable sddm.service
